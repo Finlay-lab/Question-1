b877f0c40f71bd45c3ef2e2f4550274f36590b04
Finlay-lab <85559037+Finlay-lab@users.noreply.github.com>
2022-11-03 19:56:54 +0300

    Add files via upload

---
+++ Question 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //declaration
+int Fahrenheit,Celsius;
+    //initializing
+
+
+    Celsius= (5.0/9.0) * (Fahrenheit- 32.0)
+    return 0;
+} 
